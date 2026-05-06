c515490564f31bba1582889cc8e10968b332246a
Ezequiel Gonzalez <83894961+ezegonzalez912@users.noreply.github.com>
2026-05-06 17:12:34 -0300

    update juego rpg

--- desafio4_juego_rpg.py
+++ desafio4_juego_rpg.py
@@ -1,14 +1,13 @@
 import argparse
 import time
 
-def escribir(texto, delay=0.025):
+def escribir(texto, delay=0.03):
     for c in texto:
         print(c, end='', flush=True)
         time.sleep(delay)
     print()
 
-def menu(pregunta, opciones):
-    print(pregunta)
+def elegir(opciones):
     for i, op in enumerate(opciones, 1):
         print(f'  [{i}] {op}')
     while True:
@@ -18,85 +17,79 @@ def menu(pregunta, opciones):
                 return r
         except ValueError:
             pass
-        print('Ingresa un numero valido')
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-n', '--nombre', default='Jugador')
 args = parser.parse_args()
-nombre = args.nombre
-aciertos = 0
 
-print('\n' + '='*55)
-escribir(f'\nBienvenido/a, {nombre}.')
-escribir('Sos una molecula de ARN Polimerasa II recien activada.')
-escribir('Mision: transcribir el gen de la insulina.')
+vida = 3
+nombre = args.nombre
+
+print()
+escribir(f'Sos {nombre}, una molecula de ARN Polimerasa II.')
+escribir('Tu mision: llegar al gen de la insulina y transcribirlo.')
+escribir('El nucleo es un laberinto y el tiempo corre...')
 input('\n[Enter para comenzar]\n')
 
-# escena 1 - promotor
-print('='*55)
-print('\nESCENA 1 - El promotor\n')
-escribir('Llega a la region promotora. Tres secuencias candidatas.')
-escribir('Tenes que identificar la caja TATA para unirte.\n')
-
-r = menu('Cual es la caja TATA?', ['ATCGAT', 'TATAAA', 'GCGCGC'])
-if r == 2:
-    escribir('Correcto! Te unis al promotor.')
-    aciertos += 1
-else:
-    escribir('No es esa. La correcta es TATAAA.')
-
-# escena 2 - transcripcion
-print('\n' + '='*55)
-print('\nESCENA 2 - Transcripcion\n')
-escribir('Empezas a leer el ADN molde y sintetizar el ARNm.')
-escribir('Molde: 3\'-TACGGGCTA-5\'')
-escribir('(recordá: en ARN la T se reemplaza por U)\n')
-
-r = menu('Cual es el ARNm resultante?', ['AUGCCCGAU', 'TACGGGCTA', 'ATGCCCGAT'])
+# --- escena 1 ---
+print('='*50)
+escribir('\nLlegas a la cromatina. El gen esta empaquetado y no podes leerlo.')
+escribir('Un complejo de histonas bloquea el acceso.\n')
+r = elegir([
+    'Esperas a que los factores de remodelacion abran la cromatina',
+    'Intentas forzar el paso tu sola'
+])
 if r == 1:
-    escribir('Perfecto! ARNm: 5\'-AUGCCCGAU-3\'')
-    aciertos += 1
+    escribir('\nLos factores de remodelacion actuan y la cromatina se abre.')
+    escribir('Encontras la region promotora: TATAAA.')
 else:
-    escribir('Regla: A->U, T->A, G->C, C->G. El ARNm es AUGCCCGAU.')
+    escribir('\nLa cromatina resiste. Perdes tiempo y una unidad de energia.')
+    escribir('Terminas esperando igual... pero con un golpe en el orgullo.')
+    vida -= 1
 
-# escena 3 - splicing
-print('\n' + '='*55)
-print('\nESCENA 3 - Procesamiento (splicing)\n')
-escribir('El ARNm tiene intrones que hay que sacar.')
-escribir('ARNm pre-procesado: [exon1] - [INTRON] - [exon2]\n')
-
-r = menu('Que partes se conservan en el ARNm maduro?', ['Los intrones', 'Los exones', 'Ambos'])
-if r == 2:
-    escribir('Exacto! Los exones se unen y el ARNm sale del nucleo.')
-    aciertos += 1
-else:
-    escribir('Los intrones se eliminan. Los exones son los que codifican.')
-
-# escena 4 - traduccion
-print('\n' + '='*55)
-print('\nESCENA 4 - Traduccion\n')
-escribir('El ARNm llega al ribosoma en el citoplasma.')
-escribir('Se lee de 5\' a 3\' en grupos de 3 nucleotidos (codones).')
-escribir('\nARNm: AUG-CCU-GAA-UAA')
-escribir('AUG=Met(inicio)  CCU=Pro  GAA=Glu  UAA=STOP\n')
-
-r = menu('Cual es la proteina resultante?', ['Met-Pro-Glu', 'Pro-Glu-Met', 'Met-Pro-Glu-STOP'])
+# --- escena 2 ---
+print('\n' + '='*50)
+escribir('\nEstas leyendo el molde de ADN y sintetizando el ARNm.')
+escribir('De repente aparece un intrón disfrazado de exón.')
+escribir('Quiere colarse en tu ARNm.\n')
+r = elegir([
+    'Lo dejas pasar, total despues se encarga el spliceosoma',
+    'Lo identificas y lo marcas para que el spliceosoma lo elimine'
+])
 if r == 1:
-    escribir('Correcto! El codon STOP termina la sintesis pero no se traduce.')
-    aciertos += 1
+    escribir('\nBuena decision estrategica. El spliceosoma corta el intron mas tarde.')
+    escribir('El ARNm queda limpio igual.')
+elif r == 2:
+    escribir('\nExcelente. El ARNm sale del nucleo sin intrones, listo para traducirse.')
+
+# --- escena 3 ---
+print('\n' + '='*50)
+escribir('\nEl ARNm cruza el poro nuclear y llega al ribosoma.')
+escribir('Empieza la traduccion. Todo va bien hasta que...')
+escribir('aparece un ARN de transferencia con el aminoacido EQUIVOCADO.\n')
+r = elegir([
+    'Confias en el ribosoma, el solo va a rechazar el tARN incorrecto',
+    'Entras en panico y detenes la traduccion'
+])
+if r == 1:
+    escribir('\nCorrecto. El ribosoma verifica el apareamiento de bases.')
+    escribir('El tARN incorrecto no encaja y se va. La proteina se sintetiza bien.')
+elif r == 2:
+    escribir('\nLa traduccion se corta. La proteina queda incompleta y se degrada.')
+    escribir('Hay que empezar de nuevo desde el ARNm.')
+    vida -= 1
+
+# --- escena final ---
+print('\n' + '='*50)
+escribir('\nEl codon STOP aparece en el ribosoma: UAA.')
+escribir('La insulina esta lista. El ribosoma libera la proteina.')
+print()
+
+if vida == 3:
+    escribir('Completaste la expresion genica sin errores. La celula te lo agradece.')
+elif vida == 2:
+    escribir('La insulina llego a destino, con algun tropiezo en el camino.')
 else:
-    escribir('El STOP no se traduce a aminoacido. La proteina es Met-Pro-Glu.')
+    escribir('La insulina existe, pero el camino estuvo lleno de obstaculos.')
 
-# final
-print('\n' + '='*55)
-print(f'\nFIN - {nombre}')
-print(f'Respondiste {aciertos}/4 correctamente en el primer intento.\n')
-
-if aciertos == 4:
-    escribir('Perfecto! Dominas el dogma central de la biologia molecular.')
-elif aciertos >= 2:
-    escribir('Buen trabajo, con un poco mas de practica lo dominas.')
-else:
-    escribir('El camino ADN -> ARN -> Proteina es largo, segui estudiando!')
-
-print('\n[ADN -> transcripcion -> ARNm -> splicing -> traduccion -> Proteina]\n')
+print('\nADN -> transcripcion -> ARNm -> splicing -> traduccion -> Proteina\n')
